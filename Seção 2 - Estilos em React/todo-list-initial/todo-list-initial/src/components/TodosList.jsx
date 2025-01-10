aa658ad3490c1afde4e15d00353bb91b3a988082
Henrique Soares <62728244+henriquesph@users.noreply.github.com>
2025-01-10 15:35:13 -0300

    reducer

--- Seção 2 - Estilos em React/todo-list-initial/todo-list-initial/src/components/TodosList.jsx
+++ Seção 2 - Estilos em React/todo-list-initial/todo-list-initial/src/components/TodosList.jsx
@@ -6,31 +6,12 @@ function TodosList() {
 
   const store = useContext(TodosContext);
 
-  function deleteHandler(id) {
-    if(confirm('Are you sure you want to delete the to-do')){
-      //store.setTodos(store.todos.filter(todo => todo.id !== id));
-      store.dispatch({
-        type: 'deleted',
-        id: id
-      });
-    }
-  }
-
-  function toggleIsDoneHandler(id) {
-    store.dispatch({
-      type: 'toggledIsDone',
-      id: id
-    });
-  }
-
   return (
     <>
         <div className="todos">
 
             {store.todos.map(todo =>
               <Todo
-                deleteTodo={(id)=> deleteHandler(id)}
-                toggleIsDone={(id)=> toggleIsDoneHandler(id)}
                 todo={todo}
                 key={todo.id}
             />
